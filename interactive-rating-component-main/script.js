25777d97d502c46057f6f6d8945af5ae5d46ff2f
paulmatheson <iampaulmatheson@gmail.com>
2023-04-12 07:29:23 -0700

    Added functionality to display chosen rating in ty-module

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -3,6 +3,7 @@ const inputs = document.querySelectorAll('input[type="radio"]')
 const form = document.querySelector('.rating-form')
 const ratingModule = document.querySelector('.rating-module')
 const tyModule = document.querySelector('.ty-module')
+let chosen
 
 inputs.forEach(input => {
     input.addEventListener("click", function () {
@@ -10,11 +11,13 @@ inputs.forEach(input => {
             label.classList.remove("selected") // remove .selected class from non-chosen rating
         })
         this.nextElementSibling.classList.add("selected") // add .selected class to chosen rating
+        chosen = this.value
     })
+
 })
 
 form.addEventListener("submit", () => {
     ratingModule.style.display = "none"
-    tyModule.style.display = "initial"
-
+    tyModule.style.display = "flex"
+    document.getElementById("user-rating").innerHTML = chosen
 })
