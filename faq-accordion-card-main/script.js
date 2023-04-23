51d39604804f507fbecf963d11c65e75edb90d8d
paulmatheson <iampaulmatheson@gmail.com>
2023-04-23 15:14:29 -0700

    Modified to only allow one li to expand at a time. Also added toggle for the active li state

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,9 +1,20 @@
 let accItem = document.querySelectorAll("li")
 
-accItem.forEach(item => {
+accItem.forEach((item) => {
     item.addEventListener("click", () => {
-        item.nextElementSibling.classList.toggle("hidden")
-        item.classList.toggle("bold")
-        console.log(item.children[0].classList.toggle("rotate"))
-    })
-})
+        const isOpen = item.classList.contains("active");
+        const accItems = document.querySelectorAll("li");
+        accItems.forEach((item) => {
+            item.nextElementSibling.classList.add("hidden");
+            item.classList.remove("bold");
+            item.classList.remove("active");
+            item.querySelector(".icon").classList.remove("rotate");
+        });
+        if (!isOpen) {
+            item.nextElementSibling.classList.remove("hidden");
+            item.classList.add("bold");
+            item.classList.add("active");
+            item.querySelector(".icon").classList.add("rotate");
+        }
+    });
+});
